07123198cd470cca8738925e5ad70c1b18e545f7
liuw <liuw5367@gmail.com>
2023-03-18 08:59:16 +0800

    uppdate

--- src/components/chat/MessageItem.tsx
+++ src/components/chat/MessageItem.tsx
@@ -24,57 +24,59 @@ export function MessageItem(props: Props) {
     <div
       key={item.id} //
       id={item.id}
-      className={`mb-3 flex flex-col ${isUser && "items-end"} space-y-1`}
+      className={`mb-10 flex flex-col ${isUser && "items-end"} space-y-1`}
     >
-      <div className={`flex flex-row space-x-2 ${isUser && "flex-row-reverse"}`}>
+      {item.time && <div className={`text-xs text-gray-500`}>{item.time}</div>}
+      <div className={`flex flex-row space-x-2 relative ${isUser && "flex-row-reverse"}`}>
         <Avatar
           size="sm"
           className={`mt-1 ${isUser ? "!bg-blue-800/60 ml-2" : "!bg-teal-600"}`}
           icon={isUser ? <IconUser size="1.3rem" stroke={1.5} /> : <IconRobot size="1.3rem" stroke={1.5} />}
         />
-        <div className="flex-1 ">
-          <div
-            className={`overflow-hidden rounded-lg py-2 px-3
+        <div
+          className={`flex-1 overflow-hidden rounded-lg py-2 px-3
               ${colorMode === "light" ? "bg-[#EDF2F7]" : "bg-[#021627]"}
               ${isUser && "whitespace-pre-wrap"}`}
-          >
-            {isUser ? (
-              <>{item.content}</>
-            ) : (
-              <div className="markdown-body" dangerouslySetInnerHTML={{ __html: item.markdown || "" }} />
-            )}
-          </div>
-          {item.id !== "-1" && (
-            <div className={`mt-1 flex ${isUser && "justify-end"}`}>
-              <div className="flex items-center space-x-1">
-                <div className={`text-xs text-gray-500`}>{item.time}</div>
-                <IconButton
-                  aria-label="Copy"
-                  variant="ghost"
-                  icon={hasCopied ? <IconClipboardCheck size="1rem" /> : <IconClipboard size="1rem" />}
-                  size="xs"
-                  onClick={() => {
-                    setClipboard(item.content);
-                    onCopy();
-                  }}
-                />
-                <IconButton
-                  aria-label="TTS"
-                  variant="ghost"
-                  icon={<IconPlayerPlay size="1rem" />}
-                  size="xs"
-                  onClick={() => onPlay?.(item)}
-                />
-                <IconButton
-                  aria-label="Delete"
-                  variant="ghost"
-                  icon={<IconTrash size="0.90rem" />}
-                  size="xs"
-                  onClick={() => onDelete?.(item)}
-                />
-              </div>
-            </div>
+        >
+          {isUser ? (
+            <>{item.content}</>
+          ) : (
+            <div className="markdown-body" dangerouslySetInnerHTML={{ __html: item.markdown || "" }} />
           )}
+        </div>
+        <div className={`absolute bottom-0 mt-1 flex ${isUser ? "justify-end right-12" : "left-10"}`}>
+          <div className="-mb-8 flex items-center space-x-1">
+            <IconButton
+              aria-label="Copy"
+              variant="ghost"
+              icon={
+                hasCopied ? (
+                  <IconClipboardCheck size="1rem" className="opacity-64" />
+                ) : (
+                  <IconClipboard size="1rem" className="opacity-64" />
+                )
+              }
+              size="xs"
+              onClick={() => {
+                setClipboard(item.content);
+                onCopy();
+              }}
+            />
+            <IconButton
+              aria-label="TTS"
+              variant="ghost"
+              icon={<IconPlayerPlay size="1rem" className="opacity-64" />}
+              size="xs"
+              onClick={() => onPlay?.(item)}
+            />
+            <IconButton
+              aria-label="Delete"
+              variant="ghost"
+              icon={<IconTrash size="0.90rem" className="opacity-64" />}
+              size="xs"
+              onClick={() => onDelete?.(item)}
+            />
+          </div>
         </div>
       </div>
     </div>
