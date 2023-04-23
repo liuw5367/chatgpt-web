d6128ba2216452431b50030f1c991dc411360048
liuw <liuw5367@gmail.com>
2023-04-23 17:57:51 +0800

    update unocss config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const { presetAttributify, presetIcons, presetUno, transformerVariantGroup } = require('unocss');
 const UnoCSS = require('@unocss/webpack').default;
-const tabler = require('@iconify-json/tabler/icons.json');
 const { i18n } = require('./next-i18next.config');
 const { version } = require('./package.json');
 
@@ -11,37 +9,16 @@ const nextConfig = {
   reactStrictMode: true,
   i18n,
   webpack: (config, context) => {
-    config.plugins.push(UnoCSSPlugin());
+    config.plugins.push(UnoCSS());
     if (context.buildId !== 'development') {
       // * disable filesystem cache for build
       // * https://github.com/unocss/unocss/issues/419
       // * https://webpack.js.org/configuration/cache/
       config.cache = false;
     }
+    config.cache = false;
     return config;
   },
 };
-
-function UnoCSSPlugin() {
-  return UnoCSS({
-    presets: [
-      presetAttributify(),
-      presetUno(),
-      transformerVariantGroup(),
-      presetIcons({
-        // 添加前缀防止冲突
-        prefix: 'i-',
-        // 选择需要的图库导入 https://icon-sets.iconify.design https://icones.js.org
-        // 导入时需添加依赖库 @iconify-json/{name}
-        // https://tabler-icons.io/
-        collections: { tabler },
-        extraProperties: {
-          display: 'inline-block',
-          'vertical-align': 'middle',
-        },
-      }),
-    ],
-  });
-}
 
 module.exports = nextConfig;
