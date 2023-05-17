3ec89cf165a18e667cdeb00ea8d2bf81a6f8d143
liuw <liuw5367@gmail.com>
2023-05-17 17:54:08 +0800

    编辑器代码提示卡顿问题

--- src/env.d.ts
+++ src/env.d.ts
@@ -19,10 +19,3 @@ declare global {
     }
   }
 }
-
-import type { AttributifyAttributes, AttributifyNames } from '@unocss/preset-attributify';
-
-declare module 'react' {
-  // Unocss Attributify with Prefix
-  interface HTMLAttributes<T> extends AttributifyAttributes, Partial<Record<AttributifyNames<'un-'>, string>> {}
-}
